7ced85ff31bcf6484adcdaa7268d3f4dad0cae3f
zinovevvs <ziryanov97@gmail.com>
2024-11-20 12:27:35 +0500

    first commit

---
+++ task1/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Buyer(models.Model):
+    name = models.CharField(max_length=100)
+    balance = models.DecimalField(max_digits=10, decimal_places=2)
+    age = models.IntegerField()
+
+
+class Game(models.Model):
+    title = models.CharField(max_length=200)
+    cost = models.DecimalField(max_digits=10, decimal_places=2)
+    size = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    age_limited = models.BooleanField(default=False)
+    buyers = models.ManyToManyField(Buyer, related_name='games')
