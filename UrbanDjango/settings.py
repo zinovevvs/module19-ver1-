0d3da0d562173d282a56d68b8a6bf25b1c8f8146
zinovevvs <ziryanov97@gmail.com>
2024-11-24 22:18:31 +0500

    add БД postgeSQL

--- UrbanDjango/settings.py
+++ UrbanDjango/settings.py
@@ -74,10 +74,20 @@ WSGI_APPLICATION = 'UrbanDjango.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "zinovevvs",
+        "USER": "postgres",
+        "PASSWORD": "",
+        "HOST": "localhost",
+        "PORT": "",
     }
 }
 
